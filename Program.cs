4ff4466297cb7c380e1c79e7c005a1eef2f3cc56
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-13 18:39:53 -0300

    Resolução do exercício Nomecompleto

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDoUsuario;
+            string sobreNomeDoUsuario; 
+            string nomeCompleto;
+            String Catalogo; 
+            
+            Console.Write("Por favor, digite seu primeiro: ");
+            nomeDoUsuario = Console.ReadLine();
+            
+            Console.Write("Agora, digite seu sobrenome: ");
+            sobreNomeDoUsuario = Console.ReadLine();
+            
+            nomeCompleto = $"{nomeDoUsuario} {sobreNomeDoUsuario}";
+            Catalogo = $"{sobreNomeDoUsuario.ToUpper()}, {nomeDoUsuario}";
+            Console.WriteLine();
+            
+            Console.WriteLine($" Nome Completo: {nomeCompleto} ");
+            Console.Write($" Nome do Catálago: {Catalogo} \n OBRIGADO");
+            Console.WriteLine();
+            
+            
+        }
+    }
+}
